f88033d16709e6f34ab801741b176f623a7eb761
li ying <1689710768@qq.com>
2025-11-20 10:56:04 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,14 @@
 
+// 使用js 实现树的先中后序遍历
+function TreeNode(val, left = null, right = null) {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+}
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log(root);
