ed170686652764effa33fcb6c9d49b13b44c46cc
li ying <1689710768@qq.com>
2025-11-20 11:48:15 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,4 @@
-
-// 使用js 实现树的先中后序遍历
+// 使用js 实现树的先中后序遍历,不能使用递归
 function TreeNode(val, left = null, right = null) {
   this.val = val;
   this.left = left;
@@ -11,4 +10,75 @@ root.right = new TreeNode(3);
 root.left.left = new TreeNode(4);
 root.left.right = new TreeNode(5);
 
-console.log(root);
+function preOrderTraversal(root) {
+  if (!root) {
+    return [];
+  }
+  let res = [];
+  let stack = [root];
+  while (stack.length) {
+    let top = stack.pop();
+    res.push(top.val);
+    if (top.right) {
+      stack.push(top.right);
+    }
+    if (top.left) {
+      stack.push(top.left);
+    }
+  }
+  return res;
+}
+
+function InOrderTraversal(root) {
+  if (!root) {
+    return [];
+  }
+  let res = [];
+  let stack = [root];
+  while (
+    stack.length &&
+    (!!stack[stack.length - 1].left || !!stack[stack.length - 1].right)
+  ) {
+    let top = stack.pop();
+    if (top.right) {
+      stack.push(top.right);
+    }
+    stack.push({ val: top.val });
+    if (top.left) {
+      stack.push(top.left);
+    }
+  }
+  while (stack.length) {
+    let top = stack.pop();
+    res.push(top.val);
+  }
+  return res;
+}
+function PostOrderTraversal(root) {
+  if (!root) {
+    return [];
+  }
+  let res = [];
+  let stack = [root];
+  while (
+    stack.length &&
+    (!!stack[stack.length - 1].left || !!stack[stack.length - 1].right)
+  ) {
+    let top = stack.pop();
+    stack.push({ val: top.val });
+    if (top.right) {
+      stack.push(top.right);
+    }
+    if (top.left) {
+      stack.push(top.left);
+    }
+  }
+  while (stack.length) {
+    let top = stack.pop();
+    res.push(top.val);
+  }
+  return res;
+}
+console.log(InOrderTraversal(root));
+console.log(preOrderTraversal(root));
+console.log(PostOrderTraversal(root));
