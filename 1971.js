f17883023cdd4c27dd62cc58a27434fdb1cbd7b3
li ying <1689710768@qq.com>
2025-11-28 15:27:38 +0800

    Create 1971.js

---
+++ 1971.js
@@ -0,0 +1,57 @@
+var validPath = function (n, edges, source, destination) {
+  const graph = {};
+  edges.forEach((edge) => {
+    if (graph[edge[0]]) {
+      graph[edge[0]].push(edge[1]);
+    } else {
+      graph[edge[0]] = [edge[1]];
+    }
+    if (graph[edge[1]]) {
+      graph[edge[1]].push(edge[0]);
+    } else {
+      graph[edge[1]] = [edge[0]];
+    }
+  });
+  let queue = [source];
+  let visited = new Set();
+  while (queue.length) {
+    let node = queue.shift();
+    if (visited.has(node)) continue;
+    visited.add(node);
+    if (node === destination) {
+      return true;
+    }
+    for (let neighbor of graph[node]) {
+      queue.push(neighbor);
+    }
+  }
+  return false;
+};
+
+console.log(
+  validPath(
+    3,
+    [
+      [0, 1],
+      [1, 2],
+      [2, 0],
+    ],
+    0,
+    2
+  )
+); // true
+
+console.log(
+  validPath(
+    6,
+    [
+      [0, 1],
+      [0, 2],
+      [3, 5],
+      [5, 4],
+      [4, 3],
+    ],
+    0,
+    5
+  )
+); // false
