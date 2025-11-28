a59d6e0145f98ea8cf6a99b6e6ded1b2d606c6a8
li ying <1689710768@qq.com>
2025-11-28 15:44:17 +0800

    Update 1971.js

--- 1971.js
+++ 1971.js
@@ -1,3 +1,35 @@
+// 深度优先遍历才能通过，广度优先遍历会超时
+
+// 深度优先遍历
+var validPath = function (n, edges, source, destination) {
+    const graph = {};
+    edges.forEach((edge) => {
+        if (graph[edge[0]]) {
+            graph[edge[0]].push(edge[1]);
+        } else {
+            graph[edge[0]] = [edge[1]];
+        }
+        if (graph[edge[1]]) {
+            graph[edge[1]].push(edge[0]);
+        } else {
+            graph[edge[1]] = [edge[0]];
+        }
+    });
+    let visited = new Set();
+    function dfs(node) {
+        if (node === destination) {
+            return true
+        }
+        if (visited.has(node)) return
+        visited.add(node)
+        for (let neighbor of graph[node]) {
+            if (dfs(neighbor)) return true;
+        }
+        return false
+    }
+    return dfs(source)
+};
+// 广度优先遍历
 var validPath = function (n, edges, source, destination) {
   const graph = {};
   edges.forEach((edge) => {
