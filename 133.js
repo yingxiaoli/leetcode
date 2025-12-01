665b3e01b709fadd43bfd33bc4293b839249e333
li ying <1689710768@qq.com>
2025-12-01 15:03:04 +0800

    Update 133.js

--- 133.js
+++ 133.js
@@ -1,16 +1,35 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * visited：key为原始节点，value为克隆节点
+ * queue里一直推送的是原始节点
+ * 函数返回是克隆节点，其中neighbors中不停嵌套，包含了整张图的信息
+ */
 var cloneGraph = function (node) {
-  const graph = [];
-  const queue = [node];
-  const visited = new Set();
-  while (queue.length) {
-    const n = queue.shift();
-    if (visited.has(n)) return;
-    visited.add(n);
-    let arr = [];
-    for (let neighbor of node.neighbors) {
-      queue.push(neighbor);
-      arr.push(neighbor.val);
-    }
-    graph[node.val] = arr;
+  if (!node) {
+    return node;
   }
+
+  const visited = new Set();
+  const clone = new Node(node.val, []);
+  visited.set(node, clone);
+  const queue = [node];
+  while (queue.length) {
+    const cur = queue.shift();
+    const cloneNode = visited.get(cur);
+    for (let neighbor of cur.neighbors) {
+      if (!visited.has(neighbor)) {
+        visited.set(neighbor, new Node(neighbor.val, []));
+        queue.push(neighbor);
+      }
+      cloneNode.neighbors.push(visited.get(neighbor));
+    }
+  }
+  return clone;
 };
