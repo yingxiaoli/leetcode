9cb561f64f93bdf00f3b49db99a4776e4eadf0b4
li ying <1689710768@qq.com>
2025-12-04 19:34:36 +0800

    Update heap.js

--- heap.js
+++ heap.js
@@ -1,9 +1,42 @@
+/*
 数组堆排序
 1. 从第一个非叶子节点开始堆化，直到根节点是最大值
 2. 将根节点放到最后一个位置（交换），然后再对前n-1个元素进行排序
 3.从根节点开始下滤，直到恢复堆结构，此时，根节点又是当前最大了，再重复第2步
 
 直到完成
+*/
+// 使用最大堆，给数组升序排序
+function maxHeapSort(arr) {
+  let len = arr.length;
+  for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
+    maxHeap(arr, len, i);
+  }
+  for (let i = len - 1; i >= 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    maxHeap(arr, i, 0);
+  }
+  return arr;
+}
+
+const maxHeap = (arr, size, index) => {
+  const left = 2 * index + 1;
+  const right = 2 * index + 2;
+  let largest = index;
+  if (left < size && arr[left] > arr[largest]) {
+    largest = left;
+  }
+  if (right < size && arr[right] > arr[largest]) {
+    largest = right;
+  }
+  if (largest !== index) {
+    [arr[largest], arr[index]] = [arr[index], arr[largest]];
+    maxHeap(arr, size, largest);
+  }
+};
+const arr = [2, 12, 3, 4, 9];
+console.log(maxHeapSort(arr));
+  
 =========================================================
 //小顶堆
 class MinHeap {
